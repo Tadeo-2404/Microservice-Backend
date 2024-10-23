e91fda5e53af40ef3c84667ddd44beb456a1bff5
Tadeo Alvarez <tadeo.regalado.alvarez@gmail.com>
2024-10-23 13:46:15 -0600

    adding Jenkins_pull to product_service

--- product-service/src/main/java/microservices/product_service/controller/ProductController.java
+++ product-service/src/main/java/microservices/product_service/controller/ProductController.java
@@ -1,6 +1,5 @@
 package microservices.product_service.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,6 @@ import microservices.product_service.service.Implementation.ProductServiceImpl;
 public class ProductController {
     private ProductServiceImpl productServiceImpl;
 
-    @Autowired
     public ProductController(ProductServiceImpl productServiceImpl) {
         this.productServiceImpl =  productServiceImpl;
     }
